b01fbf20a495f6af28d8ce6e4befe4faf9c40570
Bucky Kittinger <larrykittinger@gmail.com>
2018-12-24 18:16:23 -0500

    added support for eosio::wasm_import and llvm function attribute and wasm object generation

--- lib/Object/WasmObjectFile.cpp
+++ lib/Object/WasmObjectFile.cpp
@@ -300,6 +300,18 @@ Error WasmObjectFile::parseSection(WasmSection &Sec) {
   }
 }
 
+Error WasmObjectFile::parseAllowedSection(ReadContext &Ctx) {
+  while (Ctx.Ptr < Ctx.End) {
+    StringRef Name = readString(Ctx);
+    AllowedImports.push_back(Name);
+  }
+
+  if (Ctx.Ptr != Ctx.End)
+    return make_error<GenericBinaryError>("allowed import section ended prematurely",
+                                          object_error::parse_failed);
+  return Error::success();
+}
+
 Error WasmObjectFile::parseNameSection(ReadContext &Ctx) {
   llvm::DenseSet<uint64_t> Seen;
   if (Functions.size() != FunctionTypes.size()) {
@@ -666,7 +678,10 @@ Error WasmObjectFile::parseRelocSection(StringRef Name, ReadContext &Ctx) {
 }
 
 Error WasmObjectFile::parseCustomSection(WasmSection &Sec, ReadContext &Ctx) {
-  if (Sec.Name == "name") {
+  if (Sec.Name == ".imports") {
+     if (Error Err = parseAllowedSection(Ctx))
+        return Err;
+  } else if (Sec.Name == "name") {
     if (Error Err = parseNameSection(Ctx))
       return Err;
   } else if (Sec.Name == "linking") {
