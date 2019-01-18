c228681026dfdafa16afb827aebe4d7ee3b417bc
Bucky Kittinger <larrykittinger@gmail.com>
2019-01-18 16:39:35 -0500

    update tools

--- lib/Transforms/EosioApply/EosioApply.cpp
+++ lib/Transforms/EosioApply/EosioApply.cpp
@@ -36,8 +36,6 @@ namespace {
     EosioApplyPass() : FunctionPass(ID) {}
     bool runOnFunction(Function &F) override {
        if (F.hasFnAttribute("eosio_wasm_entry")) {
-         llvm::outs() << "**FUNCTION**\n";
-//       if (F.getName().equals("apply")) {
          Function* wasm_ctors = (Function*)F.getParent()->getOrInsertFunction("__wasm_call_ctors", AttributeList{}, Type::getVoidTy(F.getContext()));
          Function* wasm_dtors = (Function*)F.getParent()->getOrInsertFunction("__cxa_finalize", AttributeList{}, Type::getVoidTy(F.getContext()), Type::getInt32Ty(F.getContext()));
 
