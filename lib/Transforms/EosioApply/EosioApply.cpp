423a4ac7a7074e139c4f4c2f786d6b71d1b0965e
larryk85 <larrykittinger@gmail.com>
2020-11-19 16:51:48 -0500

    add support for auto fill in of contract

--- lib/Transforms/EosioApply/EosioApply.cpp
+++ lib/Transforms/EosioApply/EosioApply.cpp
@@ -47,6 +47,12 @@ namespace {
          IRBuilder<> builder(&F.getEntryBlock());
          builder.SetInsertPoint(&(F.getEntryBlock().front()));
 
+         auto set_contract = F.getParent()->getOrInsertFunction("eosio_set_contract_name", AttributeList{}, Type::getVoidTy(F.getContext()), Type::getInt64Ty(F.getContext()));
+
+         CallInst* set_contract_call = builder.CreateCall(set_contract, {F.arg_begin()}, "");
+         if (const Function* F_ = dyn_cast<const Function>(set_contract.getCallee()->stripPointerCasts()))
+            set_contract_call->setCallingConv(F_->getCallingConv());
+
          CallInst* wasm_ctor_call = builder.CreateCall(wasm_ctors, {}, "");
          if (const Function* F_ = dyn_cast<const Function>(wasm_ctors.getCallee()->stripPointerCasts()))
             wasm_ctor_call->setCallingConv(F_->getCallingConv());
